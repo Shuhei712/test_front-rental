31e210e73294b99bb6e09aee4fb13b859ca01b30
tunakan27 <takumi.naito27@gmail.com>
2022-01-06 15:57:21 +0900

    [add] パンくずリストの追加

---
+++ components/BreadCrumbs.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <v-breadcrumbs :items="items">
+      <template v-slot:divider>
+        <v-icon>mdi-chevron-right</v-icon>
+      </template>
+    </v-breadcrumbs>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      items: [
+        {
+          text: 'トップページ',
+          disabled: false,
+          href: '/',
+        },
+        {
+          text: 'Link 1',
+          disabled: false,
+          href: 'link_1',
+        },
+        {
+          text: 'Link 2',
+          disabled: true,
+          href: 'link_2',
+        },
+      ],
+    }),
+  }
+</script>
