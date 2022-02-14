1fd89300a0e9d2cf1032f60d72f566e4b993743d
tunakan27 <takumi.naito27@gmail.com>
2022-02-14 17:20:50 +0900

    [add] トップページの仮置き

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,21 @@
 <template>
-  <div></div>
+  <div class="top">
+    <section class="main"></section>
+    <section class="test"></section>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.main {
+  width: 100%;
+  min-height: 100vh;
+  background-image: url('~@/assets/img/top/main.png');
+  background-position: center;
+  background-size: cover;
+}
+
+.test {
+  width: 100%;
+  height: 200vh;
+}
+</style>
