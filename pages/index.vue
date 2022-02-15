1c37a93d921a339bd4a4b194de200130b9389a30
tunakan27 <takumi.naito27@gmail.com>
2022-02-15 15:46:21 +0900

    [add] カテゴリーリストコンポーネントを読み込む

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,22 @@
 <template>
   <div class="top">
     <top-main></top-main>
+    <div class="top__container d-flex py-16">
+      <category-lists></category-lists>
+      <div class="content"></div>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 @import 'assets/css/common.scss';
 
+.top {
+  &__container {
+    width: 100%;
+    max-width: 1200px;
+    height: 100vh;
+    margin: 0 auto;
+  }
 }
 </style>
