2671cefe26953d1b35a27982f8f10e933f64561b
tunakan27 <takumi.naito27@gmail.com>
2022-02-15 15:45:57 +0900

    [delete] testで表示していた部分を削除

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,11 @@
 <template>
   <div class="top">
     <top-main></top-main>
-    <section class="test"></section>
   </div>
 </template>
 
 <style lang="scss" scoped>
 @import 'assets/css/common.scss';
 
-.test {
-  width: 100%;
-  height: 200vh;
 }
 </style>
