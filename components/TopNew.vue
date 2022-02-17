c47a5710f92566b5627b07111ff55fe32f5c01e6
tunakan27 <takumi.naito27@gmail.com>
2022-02-17 19:09:23 +0900

    [change] 機材カードコンポーネント作成に伴う変更

--- components/TopNew.vue
+++ components/TopNew.vue
@@ -10,46 +10,48 @@
     </div>
     <v-container class="new__content">
       <v-row class="ma-3">
-        <v-col cols="3">
-          <v-card elevation="0" link>
-            <v-img contain height="125" src="/img/new/item_01.png"></v-img>
-            <v-card-title class="text-body-2 mb-2">SAMSUNG</v-card-title>
-            <v-card-subtitle class="text-body-2" color="text"
-              >4K対応 75in液晶ディスプレイ QH75R</v-card-subtitle
-            >
-          </v-card>
-        </v-col>
-        <v-col cols="3">
-          <v-card elevation="0" link>
-            <v-img contain height="125" src="/img/new/item_02.png"></v-img>
-            <v-card-title class="text-body-2 mb-2">Roland</v-card-title>
-            <v-card-subtitle class="text-body-2" color="text"
-              >ストリーミングビデオミキサー V-02HD MKⅡ</v-card-subtitle
-            >
-          </v-card>
-        </v-col>
-        <v-col cols="3">
-          <v-card elevation="0" link>
-            <v-img contain height="125" src="/img/new/item_03.png"></v-img>
-            <v-card-title class="text-body-2 mb-2">SAMSUNG</v-card-title>
-            <v-card-subtitle class="text-body-2" color="text"
-              >32in液晶ディスプレイ QM32R</v-card-subtitle
-            >
-          </v-card>
-        </v-col>
-        <v-col cols="3">
-          <v-card elevation="0" link>
-            <v-img contain height="125" src="/img/new/item_04.png"></v-img>
-            <v-card-title class="text-body-2 mb-2">ZOOM</v-card-title>
-            <v-card-subtitle class="text-body-2" color="text"
-              >ポッドキャストレコーダー PodTrak P8</v-card-subtitle
-            >
-          </v-card>
+        <v-col v-for="(list, index) in newItemLists" :key="index" cols="3">
+          <item-card
+            :path="list.path"
+            :maker="list.maker"
+            :name="list.name"
+          ></item-card>
         </v-col>
       </v-row>
     </v-container>
   </section>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      newItemLists: [
+        {
+          path: '/img/new/item_01.png',
+          maker: 'SAMSUNG',
+          name: '4K対応 75in液晶ディスプレイ QH75R',
+        },
+        {
+          path: '/img/new/item_02.png',
+          maker: 'Roland',
+          name: 'ストリーミングビデオミキサー V-02HD MKⅡ',
+        },
+        {
+          path: '/img/new/item_03.png',
+          maker: 'SAMSUNG',
+          name: '32in液晶ディスプレイ QM32R',
+        },
+        {
+          path: '/img/new/item_04.png',
+          maker: 'ZOOM',
+          name: 'ポッドキャストレコーダー PodTrak P8',
+        },
+      ],
+    }
+  },
+}
+</script>
 
 <style lang="scss" scoped>
 @import 'assets/css/common.scss';
