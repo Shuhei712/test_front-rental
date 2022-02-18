b5a483481965b11b0b734fe80ce1a0d353dbb7c3
tunakan27 <takumi.naito27@gmail.com>
2022-02-18 15:58:01 +0900

    [change] 実行モードをspaに変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,7 @@ import colors from 'vuetify/es5/util/colors'
 
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
+  mode: 'spa',
   head: {
     titleTemplate: '%s - takenaka_rental',
     title: 'takenaka_rental',
