1c941e8de00dab945daad9d4a34a38d89857d6f0
tunakan27 <takumi.naito27@gmail.com>
2022-02-21 12:12:02 +0900

    [add] 各ページの見出し部分のコンポーネントを作成

---
+++ components/TopBar.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="top-bar">
+    <div class="bar__inner container">
+      <bread-crumbs></bread-crumbs>
+      <h2 class="text-white letter-space-03em text-h5 bold my-5">
+        {{ title }}
+      </h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import '/assets/css/common.scss';
+
+.top-bar {
+  width: 100%;
+  height: auto;
+  max-height: 280px;
+  background-color: $accent;
+  padding-top: calc(100px + 32px);
+
+  .bar__inner {
+  }
+}
+</style>
