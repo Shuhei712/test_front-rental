dbda1a4a155b63db71853e076dea6f771f805b7a
tunakan27 <takumi.naito27@gmail.com>
2022-02-21 12:13:42 +0900

    [change] ぱんくずリストのデザイン変更
    Prettierのインデント修正含む

--- components/BreadCrumbs.vue
+++ components/BreadCrumbs.vue
@@ -1,33 +1,33 @@
 <template>
-  <div>
-    <v-breadcrumbs :items="items">
+  <div class="bread-crumbs">
+    <v-breadcrumbs class="text-white" :items="items">
       <template v-slot:divider>
-        <v-icon>mdi-chevron-right</v-icon>
+        <v-icon color="#ffffff">mdi-chevron-right</v-icon>
       </template>
     </v-breadcrumbs>
   </div>
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      items: [
-        {
-          text: 'トップページ',
-          disabled: false,
-          href: '/',
-        },
-        {
-          text: 'Link 1',
-          disabled: false,
-          href: 'link_1',
-        },
-        {
-          text: 'Link 2',
-          disabled: true,
-          href: 'link_2',
-        },
-      ],
-    }),
-  }
+export default {
+  data: () => ({
+    items: [
+      {
+        text: 'トップページ',
+        disabled: false,
+        href: '/',
+      },
+      {
+        text: 'Link 1',
+        disabled: false,
+        href: '/',
+      },
+      {
+        text: 'Link 2',
+        disabled: true,
+        href: '/',
+      },
+    ],
+  }),
+}
 </script>
