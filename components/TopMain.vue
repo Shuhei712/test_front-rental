82daf02c03cb152b735ef6b63b43874ad0941e44
tunakan27 <takumi.naito27@gmail.com>
2022-03-10 16:15:56 +0900

    [change] TopMainのコンポーネントをレスポンシブ化

--- components/TopMain.vue
+++ components/TopMain.vue
@@ -5,31 +5,21 @@
       href="https://www.takenaka-co.co.jp/"
       target="_blank"
       rel="noopener noreferrer"
-      ><v-icon class="mb-2" color="#1A264B">mdi-login-variant</v-icon
-      >株式会社タケナカ</a
+      ><v-icon class="mb-2" color="#1A264B">mdi-login-variant</v-icon>株式会社タケナカ</a
     >
     <v-container class="top__inner">
-      <v-row class="main__catch width-50">
+      <v-row class="main__catch">
         <v-col cols="12">
-          <h1
-            class="text-h5 letter-space-03em text-white no-wrap mt-4 mt-xl-15"
-          >
+          <h1 class="text-h5 letter-space-03em text-white no-wrap mt-4 mt-xl-15 hidden-md-and-down">
             映像/音響機材のレンタルサービス
           </h1>
-          <h2
-            class="text-h2 bold letter-space-015em zen-kaku-gothic no-wrap text-white mt-4 mt-xl-15"
-          >
-            必要<span class="text-h3 bold letter-space-015em line-height-25em"
-              >な</span
-            >機材、<br />
-            レンタル<span
+          <h2 class="text-h2 bold letter-space-015em zen-kaku-gothic no-wrap text-white mt-4 mt-xl-15">
+            必要<span class="text-h3 bold letter-space-015em line-height-25em">な</span>機材、<br />レンタル<span
               class="text-h3 bold letter-space-015em line-height-25em"
               >で</span
             >揃います!
           </h2>
-          <p
-            class="letter-space-02em line-height-25em text-white mt-4 mt-xl-15 no-wrap"
-          >
+          <p class="letter-space-02em line-height-25em text-white mt-4 mt-xl-15 no-wrap">
             あなたが今実現したいことは何でしょう。<br />
             配信、イベント、会議…etc<br />
             大丈夫! タケナカのレンタルにおまかせください
@@ -45,47 +35,38 @@
             outlined
             dense
             prepend-inner-icon="mdi-magnify"
-            hide-details="auto"
-          ></v-text-field
+            hide-details="auto"></v-text-field
         ></v-col>
         <v-col cols="4">
           <v-btn class="text-white" color="primary" to="/products" block
-            ><v-icon class="mr-1" color="#ffffff">mdi-text-search</v-icon
-            >詳細検索</v-btn
+            ><v-icon class="mr-1" color="#ffffff">mdi-text-search</v-icon>詳細検索</v-btn
           >
         </v-col>
       </v-row>
-      <v-row class="main__menu width-50">
+      <v-row class="main__menu width-50 mb-1">
         <v-col cols="6">
           <v-btn block
-            ><img
-              class="icon mr-1"
-              src="@/assets/img/top/beginner-icon.png"
-            />はじめての方へ<v-icon color="primary"
+            ><img class="icon mr-1" src="@/assets/img/top/beginner-icon.png" />はじめての方へ<v-icon color="primary"
               >mdi-chevron-right</v-icon
             ></v-btn
           >
         </v-col>
         <v-col cols="6">
           <v-btn block
-            ><v-icon class="mr-1" color="primary"
-              >mdi-help-circle-outline</v-icon
-            >よくある質問<v-icon color="primary"
+            ><v-icon class="mr-1" color="primary">mdi-help-circle-outline</v-icon>よくある質問<v-icon color="primary"
               >mdi-chevron-right</v-icon
             ></v-btn
           >
         </v-col>
       </v-row>
     </v-container>
-    <div class="top__notice d-flex text-body-2">
+    <div class="top__notice d-flex text-body-2 mt-1 mt-xl-13">
       <div class="notice__pickup text-center">
         <span class="pickup__category text-white mr-3 px-6 py-1">CATEGORY</span>
         新型コロナウイルス対策のための土曜日休業、 時差出退勤のお知らせ
       </div>
       <div class="notice__lists pl-10">
-        <a class="text-white" href="/"
-          >お知らせ一覧へ<v-icon color="primary">mdi-chevron-right</v-icon></a
-        >
+        <a class="text-white" href="/">お知らせ一覧へ<v-icon color="primary">mdi-chevron-right</v-icon></a>
       </div>
     </div>
   </section>
@@ -97,11 +78,14 @@
 .top__main {
   position: relative;
   width: 100%;
-  min-height: 100vh;
   background-image: url('~@/assets/img/top/main.png');
   background-position: right;
   background-size: cover;
   padding-top: calc(100px + 32px);
+}
+
+.main__catch {
+  width: 50%;
 }
 
 .top__corporate-link {
@@ -114,9 +98,7 @@
 }
 
 .top__notice {
-  position: absolute;
-  bottom: 0;
-  left: 0;
+  position: relative;
   width: 100%;
   height: 40px;
   background-color: $cushion;
