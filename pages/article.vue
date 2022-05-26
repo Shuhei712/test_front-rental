ad1766297ae55d94b8edaa167a3e1a0790f085f0
tunakan27 <takumi.naito27@gmail.com>
2022-05-26 18:41:57 +0900

    [delete] カテゴリーリストのvuex化に伴う、不要なコードの削除

--- pages/article.vue
+++ pages/article.vue
@@ -2,7 +2,7 @@
   <section class="article">
     <top-bar title="特設ページ 一覧" :bread-crumbs="breadCrumbs"></top-bar>
     <div class="article__inner d-lg-flex py-16 px-3 px-lg-0">
-      <category-lists :category-lists="categoryLists"></category-lists>
+      <category-lists></category-lists>
       <div class="content ml-lg-15">
         <div class="page-link mb-16">
           <div class="page-link__heading d-flex align-center justify-center text-caption text-md-body-2 text-gray pa-2 pa-lg-0">
@@ -226,7 +226,6 @@ export default {
       //     ],
       //   },
       // ],
-      categoryLists: [],
       pageClassLists: [],
       breadCrumbs: [],
     }
@@ -234,18 +233,10 @@ export default {
   async fetch() {
     this.$store.commit('loading/changeStatus', true)
     this.setBreadCrumbs()
-    await Promise.all([this.getCategoryList(), this.getSpecialPageList()])
+    await this.getSpecialPageList()
     this.$store.commit('loading/changeStatus', false)
   },
   methods: {
-    async getCategoryList() {
-      const param = new URLSearchParams()
-      param.append('ProjectKey', this.$config.PROJECT_KEY)
-      param.append('LangType', this.$config.LANG_JAPANESE)
-      const res = await this.$axios.$post('get_category_list.php', param)
-      // console.log(res)
-      this.categoryLists = res.CategoryRootList
-    },
     async getSpecialPageList() {
       const param = new URLSearchParams()
       param.append('ProjectKey', this.$config.PROJECT_KEY)
