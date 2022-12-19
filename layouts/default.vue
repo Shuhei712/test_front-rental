2c6a174656ff8c2faf847b8f9aef3c46cceb409f
harada_yukino <harada_yukino@takenaka-co.co.jp>
2022-12-19 10:51:35 +0900

    [Fix]初回トークンチェック　リファラとのドメイン比較で

--- layouts/default.vue
+++ layouts/default.vue
@@ -18,5 +18,25 @@ export default {
       return this.$store.getters['loading/getStatus']
     },
   },
+  async mounted(){
+    const loginID = this.$store.getters["auth/getUser"]
+    if(!loginID) return true
+    console.log('member ref')
+    const ref = document.referrer
+    const hereHost = window.location.hostname
+    const sStr = "^https?://" + hereHost
+    const rExp = new RegExp( sStr, "i" )
+    if( ref.length === 0 || !ref.match( rExp ) ) {
+      const res = await this.$checkToken()
+      console.log(ref + 'から来ました')
+      if( res.data.Status === 'FALSE' ){
+        console.log(ref + 'FALSE')
+        this.$store.dispatch('auth/resetUser')
+        if(this.$route.path.match(/myaccount/)){
+          this.$router.push('/login')
+        }
+      }
+    }
+  }
 }
 </script>
--- middleware/auth.js
+++ middleware/auth.js
@@ -13,14 +13,6 @@ export default ({ store, route, redirect, $memberAxios,$checkToken,$getAccessTok
     console.log(res.data)
     return res
   }
-  if (process.server) {
-    console.log('SSR')
-    alert('SSR')
-    // SSRでの認証
-  } else {
-    // CSRでの認証処理
-    console.log(process.server)
-  }
 
   if (!token || !loginID) {
     console.log('not member')
@@ -28,31 +20,7 @@ export default ({ store, route, redirect, $memberAxios,$checkToken,$getAccessTok
     if (route.path.match(/myaccount/)) {
       redirect('/login');
     }
-  } else {
-    console.log('member')
-    if (route.path.match(/login/)) {
+  } else if (route.path.match(/login/)) {
       redirect('/myaccount');
-    }
-
-    // const resCheckToken = await $checkToken()
-    // if(resCheckToken.data.Status === 'TRUE'){
-    //   const resGetAccessToken = await $getAccessToken()
-    //   if(resGetAccessToken.data.Status === 'TRUE'){
-    //     store.commit("auth/setAccessToken", resGetAccessToken.data.AccessToken)
-    //     const resGetLoginInfo = await getLoginInfo()
-    //     if( resGetLoginInfo.data.Status === 'TRUE' && route.path === '/login'){
-    //       redirect('/');
-    //     }else if(resGetLoginInfo.data.Status === 'FALSE'){
-    //       store.dispatch('auth/resetUser')
-    //       redirect('/login');
-    //     }
-    //   }
-    // }else{
-    //   console.log('invalid token')
-    //   store.dispatch('auth/resetUser')
-    //   if(route.path !== '/login'){
-    //     redirect('/login');
-    //   }
-    // }
   }
 }
