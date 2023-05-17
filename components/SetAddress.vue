b5b78987100bfc964ab8e138bcf4fc76ec034e84
harada_yukino <harada_yukino@takenaka-co.co.jp>
2023-05-17 18:02:13 +0900

    [Fix]住所検索：FF/Safariに対応。axios→fetch()

--- components/SetAddress.vue
+++ components/SetAddress.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <span class="red--text caption">{{ searchResult }}</span>
     <ValidationProvider
       v-slot="{ errors }"
       name="ZipCode"
@@ -86,6 +87,7 @@ export default {
       prefectArr: [
         '北海道','青森県','岩手県','宮城県','秋田県','山形県','福島県','茨城県','栃木県','群馬県','埼玉県','千葉県','東京都','神奈川県','新潟県','富山県','石川県','福井県','山梨県','長野県','岐阜県','静岡県','愛知県','三重県','滋賀県','京都府','大阪府','兵庫県','奈良県','和歌山県','鳥取県','島根県','岡山県','広島県','山口県','徳島県','香川県','愛媛県','高知県','福岡県','佐賀県','長崎県','熊本県','大分県','宮崎県','鹿児島県','沖縄県','その他'
       ],
+      searchResult: null
     }
   },
   computed:{
@@ -123,20 +125,23 @@ export default {
     async checkAddress(zipCode){
       this.loading = true
       const url = 'https://zipcloud.ibsnet.co.jp/api/search?zipcode='
-      await this.$axios.get(url + zipCode).then(res => {
-        if (res.data.status !== 200 || !res.data.results) {
-          this.prefect = ''
-          this.address = ''
-          this.loading = false
-          return
-        }
-        this.prefect = res.data.results[0].address1
-        this.address = res.data.results[0].address2+res.data.results[0].address3
-      }).catch(() => {
+      await fetch( url + zipCode )
+        .then(response => response.json())
+        .then(data => {
+          if (data.status !== 200 || !data.results) {
+            this.prefect = ''
+            this.address = ''
+            this.loading = false
+            this.searchResult = '住所が見つかりません'
+            return
+          }
+          this.searchResult = null
+          this.prefect = data.results[0].address1
+          this.address = data.results[0].address2 + data.results[0].address3
+        }).catch(() => {
         // console.log(err)
-        return false
-      })
-
+          return false
+        })
       this.loading = false
     },
 
